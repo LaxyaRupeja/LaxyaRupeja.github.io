c280da3e71e9f68bf56b935209a61a9121b00449
LaxyaRupeja <altlakshi@gmail.com>
2023-06-04 23:26:12 +0530

    second draft

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ ham.addEventListener("click", () => {
     ham.classList.toggle("open")
     mobNav.classList.toggle("is-active")
 })
+let mobNavLink = document.querySelectorAll(".mobile-nav > a");
+for (let i = 0; i < mobNavLink.length; i++) {
+    mobNavLink[i].addEventListener("click", () => {
+        ham.classList.toggle("open")
+        mobNav.classList.toggle("is-active")
+    })
+}
