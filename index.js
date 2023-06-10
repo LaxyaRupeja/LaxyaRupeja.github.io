497aa722ec2999c7a642912473c180a9828de7d1
Laxya Rupeja <121308243+LaxyaRupeja@users.noreply.github.com>
2023-06-10 22:22:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ for (let i = 0; i < mobNavLink.length; i++) {
         mobNav.classList.toggle("is-active")
     })
 }
-document.getElementById("resume-button-2").addEventListener("click", () => {
-    window.open("https://drive.google.com/file/d/1a7v8dgS9Gnak0lt-ixIYK9SevYSU2Z6b/view?usp=sharing", "__blank")
-})
+// document.getElementById("resume-button-2").addEventListener("click", () => {
+//     window.open("https://drive.google.com/file/d/1a7v8dgS9Gnak0lt-ixIYK9SevYSU2Z6b/view?usp=sharing", "__blank")
+// })
